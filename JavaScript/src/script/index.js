2cd16515f9b09d6bfb2bd07461ff65c5e50fb025
ZaidanFathan <Goldhair243@gmail.com>
2025-10-09 12:37:11 +0700

    feature form validation

--- JavaScript/src/script/index.js
+++ JavaScript/src/script/index.js
@@ -36,3 +36,57 @@ resetBtn.addEventListener("click", () => {
   }
   searchBar.focus();
 });
+
+function submitFeedback(event) {
+  const err = [];
+  const input = document.querySelectorAll("form input");
+  const text_area = document.querySelector("form textarea");
+  event.preventDefault();
+  let alertContainer = document.getElementsByClassName("alert-container");
+  const alertElement = document.querySelectorAll(".alert-container div");
+
+  //   alertContainer[0].childNodes.forEach((element) => {
+  //     if (err.indexOf(element.id) === -1) {
+  //       element.remove();
+  //     }
+  //   });
+
+  if (!text_area.value) err.push(text_area.id);
+  input.forEach((element) => {
+    if (!element.value && element.attributes.type.value !== "submit") {
+      err.push(element.attributes.type.value);
+    }
+  });
+
+  alertElement.forEach((element) => {
+    element.remove();
+  });
+
+  if (err.length > 0) {
+    err.forEach((err_field) => {
+      const div = document.createElement("div");
+      const span = document.createElement("span");
+      const p = document.createElement("p");
+      div.className = "alert";
+      div.id = err_field;
+      const paragraph_element_text = document.createTextNode(
+        `${err_field} Tidak boleh kosong, Error! Harap check kembali`
+      );
+      span.className = "closebtn";
+      span.innerHTML = "&times;";
+      span.setAttribute("onclick", "close_button(this)");
+
+      p.appendChild(paragraph_element_text);
+
+      div.appendChild(span);
+      div.appendChild(p);
+      alertContainer[0].appendChild(div);
+    });
+  }
+  console.log(err);
+
+  err.length = 0; // reset error
+}
+function close_button(element) {
+  element.parentElement.remove();
+}
